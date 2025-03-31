7299afff9d60ac125a228412eb55ba88595c07a7
Iza-04 <Iza.m150492@gmail.com>
2025-03-31 21:03:12 +0500

    conditions have been added

--- src/index.js
+++ src/index.js
@@ -130,6 +130,13 @@ function Footer() {
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
   // else alert("Sorry we're closed");
 
+  if (!isOpen)
+    return (
+      <p>
+        We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+      </p>
+    );
+
   return (
     <footer className="footer">
       {isOpen ? (
